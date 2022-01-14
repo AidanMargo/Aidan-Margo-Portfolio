5452fa050ee680834f2d7c18b3a3a4bfbe4b7cc8
Aidan Margo <aidanmargo@gmail.com>
2022-01-13 20:24:41 -0600

    updated intro message

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,8 @@ function Intro({scrollTo}){
       <div class="intro">
         <h3>Hi, <span id='name'>I'm Aidan</span>, and</h3>
         <h1>I help people create things.</h1>
-        <p id="bio">I'm a software engineering student with big dreams to change the world through technology
-          and development. Currently developing my back-end skills in Ruby on Rails and searching for entry-level software engineer roles.
+        <p id="bio">I'm a software engineer  with big dreams to change the world through technology
+          and development. Now searching for my first developer role, and beginning my dive into the world of Java.
         </p>
         <button id="hire-me" onClick={() => scrollTo('contact')}>Say Hello</button>
       </div>
